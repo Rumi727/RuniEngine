bfddc399ea44d92ba2cc9bc8793a21ace2b3cefc
Simsimhan Chobo <jun32820@gmail.com>
2023-07-28 12:18:35 +0900

    Create ThreadManager.cs

---
+++ Packages/com.teambucket.runiengine/Runtime/ThreadManager.cs
@@ -0,0 +1,18 @@
+#nullable enable
+using System.Threading;
+
+namespace RuniEngine
+{
+    public static class ThreadManager
+    {
+        /// <summary>
+        /// { get; } = Thread.CurrentThread.ManagedThreadId
+        /// </summary>
+        public static int mainThreadId { get; } = Thread.CurrentThread.ManagedThreadId;
+
+        /// <summary>
+        /// { get { mainThreadId == Thread.CurrentThread.ManagedThreadId; } }
+        /// </summary>
+        public static bool isMainThread => mainThreadId == Thread.CurrentThread.ManagedThreadId;
+    }
+}
