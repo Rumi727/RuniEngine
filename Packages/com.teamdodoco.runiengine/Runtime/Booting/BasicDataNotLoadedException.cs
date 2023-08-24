c11e29724f6991eb3e33c1b7ec620ddb12aed5b5
Simsimhan Chobo <jun32820@gmail.com>
2023-08-24 19:03:23 +0900

    반복된 코드 제거를 위한 Exception 메소드 추가

--- Packages/com.teamdodoco.runiengine/Runtime/Booting/BasicDataNotLoadedException.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Booting/BasicDataNotLoadedException.cs
@@ -5,6 +5,12 @@ namespace RuniEngine.Booting
 {
     public class BasicDataNotLoadedException : Exception
     {
+        public static void Exception()
+        {
+            if (!BootLoader.basicDataLoaded)
+                throw new BasicDataNotLoadedException();
+        }
+
         public BasicDataNotLoadedException() : base("기초 데이터가 로드되지 않은 상태에서 작업을 진행할 수 없습니다")
         {
 
