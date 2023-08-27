7a88fe1772775b92b2a398c3fccfcbd9a2fd65d5
Simsimhan Chobo <jun32820@gmail.com>
2023-08-27 16:08:11 +0900

    에디터 언어 추가

---
+++ Packages/com.teamdodoco.runiengine/Editor/Utility/EditorTool.Language.cs
@@ -0,0 +1,61 @@
+#nullable enable
+using Newtonsoft.Json;
+using RuniEngine.Data;
+using RuniEngine.Json;
+using RuniEngine.Resource;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RuniEngine.Editor
+{
+    public partial class EditorTool
+    {
+        [ProjectData]
+        public sealed class ProjectData
+        {
+            [JsonProperty] public static string currentLanguage { get; set; } = "en_us";
+        }
+
+        public const string packageLanguagePath = packageEditorPath + "/Languages";
+
+        /// <summary>
+        /// value = loadedLanguages[language][key];
+        /// </summary>
+        static readonly Dictionary<string, Dictionary<string, string>> loadedLanguages = new();
+
+        static StorableClass? storableClass;
+        public static string? GetText(string key, string language = "")
+        {
+            if (string.IsNullOrWhiteSpace(language))
+            {
+                if (storableClass == null)
+                {
+                    storableClass = new StorableClass(typeof(ProjectData));
+                    storableClass.AutoNameLoad(Kernel.projectDataPath);
+                }
+
+                language = ProjectData.currentLanguage;
+            }
+
+            if (loadedLanguages.TryGetValue(language, out var result) && result.TryGetValue(key, out string value))
+                return value;
+            else if (ResourceManager.FileExtensionExists(Path.Combine(packageLanguagePath, language), out string outPath, ExtensionFilter.textFileFilter))
+            {
+                Dictionary<string, string>? lang = JsonManager.JsonRead<Dictionary<string, string>>(outPath);
+                if (lang == null)
+                    return null;
+
+                if (lang.TryGetValue(key, out value))
+                {
+                    loadedLanguages.TryAdd(language, new());
+                    loadedLanguages[language].TryAdd(key, value);
+
+                    return value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
