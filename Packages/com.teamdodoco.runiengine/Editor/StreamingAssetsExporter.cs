a910d5b8c443dfad8fac91261bb292f7157c93d9
Simsimhan Chobo <jun32820@gmail.com>
2023-08-27 20:46:15 +0900

    StreamingAssetsExporter 클래스 추가

---
+++ Packages/com.teamdodoco.runiengine/Editor/StreamingAssetsExporter.cs
@@ -0,0 +1,18 @@
+#nullable enable
+using RuniEngine.Resource;
+using System.IO;
+using UnityEditor;
+
+namespace RuniEngine.Editor
+{
+    public static class StreamingAssetsExporter
+    {
+        [MenuItem("Runi Engine/Streaming Assets Export")]
+        public static void StreamingAssetsExport()
+        {
+            AssetDatabase.ExportPackage(Path.Combine("Assets", Kernel.streamingAssetsFolderName, ResourceManager.rootName),
+                Path.Combine(EditorTool.packageResourcesPath, Kernel.streamingAssetsFolderName + ".unitypackage"),
+                ExportPackageOptions.Recurse);
+        }
+    }
+}
