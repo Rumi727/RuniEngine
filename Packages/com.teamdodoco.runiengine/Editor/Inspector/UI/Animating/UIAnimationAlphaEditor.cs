2dc7f19d91d76c8d959a743bcc47ad767bdd1d29
Simsimhan Chobo <jun32820@gmail.com>
2023-08-30 21:23:35 +0900

    UIAnimationAlpha 추가

---
+++ Packages/com.teamdodoco.runiengine/Editor/Inspector/UI/Animating/UIAnimationAlphaEditor.cs
@@ -0,0 +1,22 @@
+#nullable enable
+using RuniEngine.UI.Animating;
+using UnityEditor;
+
+namespace RuniEngine.Editor.Inspector.UI.Animating
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(UIAnimationAlpha))]
+    public class UIAnimationAlphaEditor : UIAnimationEditor<UIAnimationAlpha>
+    {
+        public override void OnInspectorGUI()
+        {
+            if (target == null)
+                return;
+
+            base.OnInspectorGUI();
+            Space();
+
+            UseProperty("_alpha", TryGetText("gui.alpha"));
+        }
+    }
+}
---
+++ Packages/com.teamdodoco.runiengine/Runtime/UI/Animating/UIAnimationAlpha.cs
@@ -0,0 +1,27 @@
+#nullable enable
+using System.Linq;
+using UnityEngine;
+
+namespace RuniEngine.UI.Animating
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UIAnimationAlpha : UIAnimation
+    {
+        public CanvasGroup? canvasGroup => _canvasGroup = this.GetComponentFieldSave(_canvasGroup);
+        CanvasGroup? _canvasGroup;
+
+        public AnimationCurve alpha => _alpha;
+        [SerializeField] AnimationCurve _alpha = new AnimationCurve();
+
+        public override float length => GetCurveLength(alpha);
+
+        public override void LayoutUpdate()
+        {
+            UIAnimator? animator = this.animator;
+            if (animator == null || canvasGroup == null)
+                return;
+
+            canvasGroup.alpha = alpha.Evaluate(animator.time);
+        }
+    }
+}
