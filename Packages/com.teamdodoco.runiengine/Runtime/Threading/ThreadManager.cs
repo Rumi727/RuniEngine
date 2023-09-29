4b66c67f63fde491812e2f5a34c2b7aa0a8fe593
Simsimhan Chobo <jun32820@gmail.com>
2023-09-30 00:30:54 +0900

    Interlocked 클래스를 사용한 스레드 잠금을 메소드로 통합

--- Packages/com.teamdodoco.runiengine/Runtime/Threading/ThreadManager.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Threading/ThreadManager.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using RuniEngine.Resource;
 using System.Threading;
 
 namespace RuniEngine.Threading
@@ -14,5 +15,19 @@ namespace RuniEngine.Threading
         /// { get { mainThreadId == Thread.CurrentThread.ManagedThreadId; } }
         /// </summary>
         public static bool isMainThread => mainThreadId == Thread.CurrentThread.ManagedThreadId;
+
+        public static void Lock(ref int location)
+        {
+            Debug.ForceLog(LanguageLoader.TryGetText("thread_manager.lock", Debug.NameOfCallingClass()));
+
+            while (Interlocked.CompareExchange(ref location, 1, 0) == 0)
+                Thread.Sleep(1);
+        }
+
+        public static void Unlock(ref int location)
+        {
+            Debug.ForceLog(LanguageLoader.TryGetText("thread_manager.unlock").Replace("{class}", Debug.NameOfCallingClass()));
+            Interlocked.Exchange(ref location, 0);
+        }
     }
 }
