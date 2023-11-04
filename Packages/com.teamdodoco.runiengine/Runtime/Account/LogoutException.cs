179b745a6e0267b95072d079e54bc49ce120d006
Simsimhan Chobo <jun32820@gmail.com>
2023-11-04 19:00:01 +0900

    반환값이 LogoutException 클래스가 아닌것을 수정

--- Packages/com.teamdodoco.runiengine/Runtime/Account/LogoutException.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Account/LogoutException.cs
@@ -6,8 +6,10 @@ namespace RuniEngine.Account
 {
     public class LogoutException : Exception
     {
-        public static Exception AlreadyLoggedException() => new LogoutException(LanguageLoader.TryGetText("exception.already_logged"));
-        public static Exception LoggedOutUserDataException() => new LogoutException(LanguageLoader.TryGetText("exception.logged_out_user_data"));
+        /// <returns><see cref="LogoutException"/> 이미 로그아웃되었습니다</returns>
+        public static LogoutException AlreadyLoggedException() => new LogoutException(LanguageLoader.TryGetText("exception.already_logged"));
+        /// <returns><see cref="LogoutException"/> 로그아웃된 상태에서 유저 데이터에 접근할 수 없습니다</returns>
+        public static LogoutException LoggedOutUserDataException() => new LogoutException(LanguageLoader.TryGetText("exception.logged_out_user_data"));
 
         public LogoutException(string message) : base(message)
         {
