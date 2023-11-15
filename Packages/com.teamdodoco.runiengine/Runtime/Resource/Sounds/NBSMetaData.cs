0a8b53bd339ebcc2d6f264fc22763f9e91d75fdb
Rumi727 <jun32820@gmail.com>
2023-11-15 22:39:42 +0900

    loopStartTime의 값을 NBS 파일 내부에 있는 값을 사용하게 변경

--- Packages/com.teamdodoco.runiengine/Runtime/Resource/Sounds/NBSMetaData.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Resource/Sounds/NBSMetaData.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using Newtonsoft.Json;
 using RuniEngine.NBS;
 
 namespace RuniEngine.Resource.Sounds
@@ -6,6 +7,8 @@ namespace RuniEngine.Resource.Sounds
     public sealed class NBSMetaData : SoundMetaDataBase
     {
         public NBSMetaData(string path, double pitch, double tempo, bool stream, double loopStartTime, double loopOffsetTime, NBSFile nbsFile) : base(path, pitch, tempo, stream, loopStartTime, loopOffsetTime) => this.nbsFile = nbsFile;
+
+        [JsonIgnore] public override double loopStartTime => nbsFile.loopStartTick / 20d;
 
         public NBSFile nbsFile { get; }
     }
