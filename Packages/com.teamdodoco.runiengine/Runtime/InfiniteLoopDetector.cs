e1b536c9f9e567583b6a96091d3ec4e324a33e22
Rumi727 <jun32820@gmail.com>
2023-11-19 19:44:17 +0900

    Create InfiniteLoopDetector.cs

---
+++ Packages/com.teamdodoco.runiengine/Runtime/InfiniteLoopDetector.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace RuniEngine
+{
+    /// <summary> 무한 루프 검사 및 방지 (에디터 전용)</summary>
+    public static class InfiniteLoopDetector
+    {
+        static string prevPoint = "";
+        static int detectionCount = 0;
+        const int detectionThreshold = 10000;
+
+        [Conditional("UNITY_EDITOR")]
+        public static void Run([CallerMemberName] string mn = "", [CallerFilePath] string fp = "", [CallerLineNumber] int ln = 0)
+        {
+            string currentPoint = $"{fp}:{ln}, {mn}()";
+
+            if (prevPoint == currentPoint)
+                detectionCount++;
+            else
+                detectionCount = 0;
+
+            if (detectionCount > detectionThreshold)
+                throw new Exception($"Infinite Loop Detected: \n{currentPoint}\n\n");
+
+            prevPoint = currentPoint;
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+        static void Init()
+        {
+            UnityEditor.EditorApplication.update += () =>
+            {
+                detectionCount = 0;
+            };
+        }
+#endif
+    }
+}
