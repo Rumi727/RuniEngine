d373a375c31b2a95f31fad1569d32a4db127fcd0
Rumi727 <jun32820@gmail.com>
2023-11-24 00:14:10 +0900

    디자인 수정

--- Packages/com.teamdodoco.runiengine/Install/WelcomeScreen.cs
+++ Packages/com.teamdodoco.runiengine/Install/WelcomeScreen.cs
@@ -14,41 +14,17 @@ namespace RuniEngine.Install
 
         public int sort { get; } = 0;
 
-        float rotation = 0;
         static GUIStyle? headStyle;
         public void DrawGUI()
         {
-            Texture2D? logo = null;
-            float timer = 0;
-            if (installerMainWindow != null)
-            {
-                logo = installerMainWindow.logoTexture;
-                timer = (float)(installerMainWindow.stopwatch.Elapsed.TotalSeconds);
-
-                installerMainWindow.Repaint();
-            }
-
+            float timer = (float)InstallerMainWindow.stopwatch.Elapsed.TotalSeconds;
             GUILayout.FlexibleSpace();
 
             {
                 GUILayout.BeginHorizontal();
                 GUILayout.FlexibleSpace();
 
-                {
-                    GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
-
-                    Rect rect = EditorGUILayout.GetControlRect(GUILayout.Width(100), GUILayout.Height(100));
-                    rotation = ((timer - timer.Repeat(0.1f)) * 32).Repeat(360);
-
-                    Matrix4x4 matrix = GUI.matrix;
-                    GUIUtility.RotateAroundPivot(rotation, rect.center);
-
-                    GUI.DrawTexture(rect, logo);
-
-                    GUI.matrix = matrix;
-                    GUI.EndGroup();
-                }
-
+                EditorGUILayout.GetControlRect(GUILayout.Width(100), GUILayout.Height(100));
                 GUILayout.Space(20);
 
                 {
