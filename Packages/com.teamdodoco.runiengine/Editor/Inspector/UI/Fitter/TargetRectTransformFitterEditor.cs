937c7db7a1fa5a9e508defc1269f595f607acd46
Rumi727 <jun32820@gmail.com>
2023-11-25 20:49:39 +0900

    경고 문구 추가

--- Packages/com.teamdodoco.runiengine/Editor/Inspector/UI/Fitter/TargetRectTransformFitterEditor.cs
+++ Packages/com.teamdodoco.runiengine/Editor/Inspector/UI/Fitter/TargetRectTransformFitterEditor.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using RuniEngine.UI.Fitter;
 using UnityEditor;
+using UnityEngine;
 
 namespace RuniEngine.Editor.Inspector.UI.Fitter
 {
@@ -10,10 +11,16 @@ namespace RuniEngine.Editor.Inspector.UI.Fitter
     {
         public override void OnInspectorGUI()
         {
+            if (targets == null || targets.Length <= 0 || target == null)
+                return;
+
             base.OnInspectorGUI();
             Space();
 
             UseProperty("_targetRectTransform", TryGetText("inspector.target_~").Replace("{target}", TryGetText("inspector.rect_transform")));
+
+            if (targets.Length <= 1 && target.rectTransform.lossyScale != Vector3.one && target.rectTransform.rotation != Quaternion.identity)
+                EditorGUILayout.HelpBox(TryGetText("inspector.target_rect_transform_fitter_editor.warning"), MessageType.Warning);
         }
     }
 }
