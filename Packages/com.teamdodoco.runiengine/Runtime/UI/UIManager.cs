54d02a1e3bc05b26c3e6b1927b5b08f34cdb7dd7
Rumi727 <jun32820@gmail.com>
2023-12-02 00:35:33 +0900

    uiSize 설정 추가

--- Packages/com.teamdodoco.runiengine/Runtime/UI/UIManager.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/UI/UIManager.cs
@@ -15,6 +15,11 @@ namespace RuniEngine.UI
 
             [JsonProperty] public static float defaultLerpAniSpeed { get => _defaultLerpAniSpeed.Clamp(0); set => _defaultLerpAniSpeed = value.Clamp(0); }
             static float _defaultLerpAniSpeed = 0.08f;
+
+            [JsonProperty] public static float uiSize { get => _uiSize.Clamp(0); set => _uiSize = value.Clamp(0); }
+            static float _uiSize = 1;
         }
+
+        public static float uiSize { get => UserData.uiSize; set => UserData.uiSize = value; }
     }
 }
