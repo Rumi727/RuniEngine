a5fc29af2f65b43ba1dd360c4edd0151de52cfde
Rumi727 <jun32820@gmail.com>
2023-12-02 02:54:11 +0900

    IEquatable 인터페이스 구현

--- Packages/com.teamdodoco.runiengine/Runtime/Structs/RectOffset.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Structs/RectOffset.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace RuniEngine
 {
     [Serializable]
-    public struct RectOffset
+    public struct RectOffset : IEquatable<RectOffset>
     {
         public RectOffset(Vector2 min, Vector2 max)
         {
@@ -17,5 +17,17 @@ namespace RuniEngine
         [FieldName("gui.max")] public Vector2 max;
 
         public static RectOffset zero => new RectOffset(Vector2.zero, Vector2.zero);
+
+
+
+        public static bool operator ==(RectOffset left, RectOffset right) => left.Equals(right);
+        public static bool operator !=(RectOffset left, RectOffset right) => !(left == right);
+
+
+
+        public readonly bool Equals(RectOffset other) => min.Equals(other.min) && max.Equals(other.max);
+        public override readonly bool Equals(object? obj) => obj is RectOffset result && Equals(result);
+
+        public override readonly int GetHashCode() => HashCode.Combine(min, max);
     }
 }
