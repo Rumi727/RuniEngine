a12c8fa057c13b961091efa8e7fc4828c8ef8247
Rumi727 <jun32820@gmail.com>
2023-12-02 02:54:11 +0900

    IEquatable 인터페이스 구현

--- Packages/com.teamdodoco.runiengine/Runtime/Structs/RectCorner.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Structs/RectCorner.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 namespace RuniEngine
 {
     [Serializable]
-    public struct RectCorner
+    public struct RectCorner : IEquatable<RectCorner>
     {
+        public RectCorner(Vector2 bottomLeft, Vector2 topLeft, Vector2 topRight, Vector2 bottomRight)
+        {
+            this.bottomLeft = bottomLeft;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomRight = bottomRight;
+        }
+
+        public RectCorner(Rect rect)
+        {
+            bottomLeft = new Vector2(rect.xMin, rect.yMin);
+            topLeft = new Vector2(rect.xMin, rect.yMax);
+            topRight = new Vector2(rect.xMax, rect.yMax);
+            bottomRight = new Vector2(rect.xMax, rect.yMin);
+        }
+
+        public RectCorner(Vector2 min, Vector2 max)
+        {
+            bottomLeft = new Vector2(min.x, min.y);
+            topLeft = new Vector2(min.x, max.y);
+            topRight = new Vector2(max.x, max.y);
+            bottomRight = new Vector2(max.x, min.y);
+        }
+
+
+
         public Rect rect
         {
             readonly get => this;
@@ -31,30 +57,21 @@ namespace RuniEngine
         public static implicit operator Rect(RectCorner value) => new Rect(value.bottomLeft, value.topRight - value.bottomLeft);
         public static implicit operator RectCorner(Rect value) => new RectCorner(value);
 
+        public static bool operator ==(RectCorner left, RectCorner right) => left.Equals(right);
+        public static bool operator !=(RectCorner left, RectCorner right) => !(left == right);
 
 
-        public RectCorner(Vector2 bottomLeft, Vector2 topLeft, Vector2 topRight, Vector2 bottomRight)
+
+        public override readonly bool Equals(object? obj) => obj is RectCorner value && Equals(value);
+
+        public readonly bool Equals(RectCorner other)
         {
-            this.bottomLeft = bottomLeft;
-            this.topLeft = topLeft;
-            this.topRight = topRight;
-            this.bottomRight = bottomRight;
+            return bottomLeft.Equals(other.bottomLeft) &&
+                   topLeft.Equals(other.topLeft) &&
+                   topRight.Equals(other.topRight) &&
+                   bottomRight.Equals(other.bottomRight);
         }
 
-        public RectCorner(Rect rect)
-        {
-            bottomLeft = new Vector2(rect.xMin, rect.yMin);
-            topLeft = new Vector2(rect.xMin, rect.yMax);
-            topRight = new Vector2(rect.xMax, rect.yMax);
-            bottomRight = new Vector2(rect.xMax, rect.yMin);
-        }
-
-        public RectCorner(Vector2 min, Vector2 max)
-        {
-            bottomLeft = new Vector2(min.x, min.y);
-            topLeft = new Vector2(min.x, max.y);
-            topRight = new Vector2(max.x, max.y);
-            bottomRight = new Vector2(max.x, min.y);
-        }
+        public override readonly int GetHashCode() => HashCode.Combine(bottomLeft, topLeft, topRight, bottomRight);
     }
 }
