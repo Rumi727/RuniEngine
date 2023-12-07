8c0af435bcd623fea20347ad5f648419cdebbb62
Rumi727 <jun32820@gmail.com>
2023-12-07 22:50:58 +0900

    볼륨이 독립적으로 동작하도록 변경

--- Packages/com.teamdodoco.runiengine.sounds/Runtime/Sounds/AudioPlayer.cs
+++ Packages/com.teamdodoco.runiengine.sounds/Runtime/Sounds/AudioPlayer.cs
@@ -133,7 +133,10 @@ namespace RuniEngine.Sounds
             if (audioSource == null || AudioLoader.audioListener == null)
                 return;
 
-            audioSource.volume = volume;
+            if (audioSource.bypassEffects)
+                audioSource.volume = volume;
+            else
+                audioSource.volume = 1;
 
             if (pitch != 0)
             {
@@ -251,206 +254,13 @@ namespace RuniEngine.Sounds
             time = 0;
         }
 
-
-
-        /*[NonSerialized] int onAudioFilterReadLock = 0;
-        [NonSerialized] float[] tempData = new float[0];
-        [NonSerialized] double tempoAdjustmentIndex = 0;
-        [NonSerialized] long realSampleIndex = 0;
         protected override void OnAudioFilterRead(float[] data, int channels)
         {
-            try
-            {
-                ThreadManager.Lock(ref onAudioFilterReadLock);
-
-                if (tempData.Length != AudioLoader.systemChannels)
-                    tempData = new float[AudioLoader.systemChannels];
-                
-                if (isPlaying && !isPaused && realTempo != 0 && audioMetaData != null && datas != null)
-                {
-                    long sampleIndex = this.sampleIndex;
-                    long realSampleIndex = Interlocked.Read(ref this.realSampleIndex);
-                    double tempo = realTempo;
-                    double pitch = realPitch;
-                    float volume = (float)this.volume;
-                    bool loop = this.loop;
-
-                    int loopStartIndex = audioMetaData.loopStartIndex;
-                    int loopOffsetIndex = audioMetaData.loopOffsetIndex;
-
-                    int audioChannels = this.channels;
-                    int samplesLength = datas.Length / audioChannels;
-                    
-                    if (pitch > 0)
-                    {
-                        for (int i = 0; i < data.Length; i += channels)
-                        {
-                            int index;
-                            if (tempo > 0)
-                                index = (int)((realSampleIndex * audioChannels) + i);
-                            else
-                                index = (int)((realSampleIndex * audioChannels) - i);
-
-                            GetAudioSample(ref tempData, datas, index, channels, audioChannels, loop, loopStartIndex * audioChannels, loopOffsetIndex * audioChannels, spatial, panStereo, spatialStereo);
-                            for (int j = 0; j < channels; j++)
-                                data[i + j] = tempData[j] * volume;
-                        }
-                    }
-
-                    //시간 조정
-                    {
-                        {
-                            double value = data.Length / audioChannels;
-                            double pitchDivideTempo = tempo / (pitch != 0 ? pitch : 1);
-
-                            realSampleIndex += (long)(value * tempo.Sign());
-
-                            //템포
-                            tempoAdjustmentIndex += value;
-
-                            double condition = 2048 * (pitch != 0 ? pitch : 1);
-                            while (tempoAdjustmentIndex >= condition)
-                            {
-                                realSampleIndex -= (long)(value * (1 - pitchDivideTempo.Abs()) * (condition / value) * tempo.Sign());
-                                sampleIndex = realSampleIndex;
-
-                                tempoAdjustmentIndex -= condition;
-                            }
-                        }
-
-                        if (loop)
-                        {
-                            bool isLooped = false;
-
-                            while (tempo >= 0 && realSampleIndex >= samplesLength)
-                            {
-                                long value = samplesLength - 1 - (loopStartIndex + loopOffsetIndex);
-
-                                sampleIndex -= value;
-                                realSampleIndex -= value;
-
-                                isLooped = true;
-                            }
-
-                            while (tempo < 0 && realSampleIndex < loopStartIndex + loopOffsetIndex)
-                            {
-                                long value = samplesLength - 1 - (loopStartIndex + loopOffsetIndex);
-
-                                sampleIndex += value;
-                                realSampleIndex += value;
-
-                                isLooped = true;
-                            }
-
-                            if (isLooped)
-                                LoopedEventInvoke();
-                        }
-                    }
-
-                    Interlocked.Exchange(ref _sampleIndex, sampleIndex);
-                    Interlocked.Exchange(ref this.realSampleIndex, realSampleIndex);
-                }
-            }
-            finally
-            {
-                ThreadManager.Unlock(ref onAudioFilterReadLock);
-            }
+            for (int i = 0; i < data.Length; i++)
+                data[i] *= volume;
 
             base.OnAudioFilterRead(data, channels);
         }
-
-        /// <summary>
-        /// 채널 개수에 영향 받지 않는 원시 인덱스를 인자로 전달해야합니다
-        /// </summary>
-        public static void GetAudioSample(ref float[] data, float[] samples, int index, int channels, int audioChannels, bool loop, int loopStartIndex, int loopOffsetIndex, bool spatial, double panStereo, double spatialStereo)
-        {
-            //현재 재생중인 오디오 채널이 2 보다 크다면 변환 없이 재생
-            if (audioChannels > 2)
-            {
-                for (int i = 0; i < channels; i++)
-                    data[i] = GetSample(channels);
-            }
-            else if (audioChannels == 2) //현재 재생중인 오디오 채널이 2일때
-            {
-                //현재 시스템 채널이 1 보다 크다면 스테레오로 재생
-                if (channels >= 2)
-                {
-                    for (int i = 0; i < channels; i++)
-                        data[i] = GetSample(i);
-                }
-                else //현재 시스템 채널이 1 이하라면 모노로 재생
-                {
-                    float left = GetSample(0);
-                    float right = GetSample(1);
-
-                    data[0] = (left + right) * 0.66666666666666f;
-                }
-            }
-            else if (audioChannels < 2) //현재 재생중인 오디오의 채널이 2 보다 작다면 변환 없이 재생
-            {
-                /* 
-                 * 오디오 품질이 구려진다
-                 * 왜 그런지는 알 것 같은데 해결 방법을 ㅁ?ㄹ
-                 * 애초에 지금 템포도 구현을 잘못해놔서 이상해지지만 구현을 제대로 하기에는 너무 어렵다
-                 * 나중에 오디오 재생 다시 설계해봐야할 듯?
-                 * 그 나중에가 언제가 될진 모르겠지만...
-                 
-
-                for (int i = 0; i < channels; i++)
-                    data[i] = GetSample(0) / channels;
-            }
-
-            if (channels >= 2)
-            {
-                float left = data[0];
-                float right = data[1];
-
-                if (!spatial)
-                {
-                    float leftStereo = (float)-panStereo.Clamp(-1, 0);
-                    float rightStereo = (float)panStereo.Clamp(0, 1);
-
-                    data[0] = (left + 0f.Lerp(right, leftStereo)) * (1 - rightStereo) * 1f.Lerp(0.5f, (float)panStereo.Abs());
-                    data[1] = (right + 0f.Lerp(left, rightStereo)) * (1 - leftStereo) * 1f.Lerp(0.5f, (float)panStereo.Abs());
-                }
-                else
-                {
-                    float leftStereo = (float)-spatialStereo.Clamp(-1, 0);
-                    float rightStereo = (float)spatialStereo.Clamp(0, 1);
-
-                    data[0] = (left + 0f.Lerp(right, leftStereo)) * (1 - rightStereo) * 1f.Lerp(0.5f, (float)spatialStereo.Abs());
-                    data[1] = (right + 0f.Lerp(left, rightStereo)) * (1 - leftStereo) * 1f.Lerp(0.5f, (float)spatialStereo.Abs());
-                }
-            }
-
-            float GetSample(int i)
-            {
-                int sampleIndex = index + i;
-                if (loop)
-                    sampleIndex = sampleIndex.Repeat(samples.Length - 1);
-                
-                if (sampleIndex >= 0 && sampleIndex < samples.Length)
-                {
-                    float sample = samples[sampleIndex];
-
-                    //루프
-                    if (loop)
-                    {
-                        int rawLoopOffsetSampleIndex = sampleIndex - (samples.Length - 1 - loopOffsetIndex);
-                        int loopOffsetSampleIndex = loopStartIndex + rawLoopOffsetSampleIndex.Repeat(samples.Length - 1 - loopStartIndex);
-                        
-                        if (rawLoopOffsetSampleIndex >= 0 && loopOffsetSampleIndex >= 0 && loopOffsetSampleIndex < samples.Length)
-                            sample += samples[loopOffsetSampleIndex];
-                    }
-
-                    return sample;
-                }
-
-                return 0;
-            }
-
-            return;
-        }*/
 
         public static AudioPlayer? PlayAudio(string key, string nameSpace = "", float volume = 1, bool loop = false, float pitch = 1, float tempo = 1, float panStereo = 0, Transform? parent = null) => InternalPlayAudio(key, nameSpace, volume, loop, pitch, tempo, panStereo, parent, false, Vector3.zero, 0, 16);
 
