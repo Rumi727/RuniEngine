e96b87e0814d56de2e962241d2d211c3381e6aef
Rumi727 <jun32820@gmail.com>
2023-12-10 03:45:37 +0900

    IRenderer 인터페이스 추가

---
+++ Packages/com.teamdodoco.runiengine/Runtime/Rendering/IRenderer.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace RuniEngine.Rendering
+{
+    public interface IRenderer
+    {
+        string nameSpace { get; set; }
+        string path { get; set; }
+
+        NameSpacePathPair pair { get; set; }
+
+        void Refresh()
+        {
+
+        }
+    }
+}
