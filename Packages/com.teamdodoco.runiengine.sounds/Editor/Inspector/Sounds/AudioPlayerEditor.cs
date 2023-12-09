ed9e85de1a4a949a1eb6a88f890948e4e78e4481
Rumi727 <jun32820@gmail.com>
2023-12-10 03:08:18 +0900

    코드 위치 이동

--- Packages/com.teamdodoco.runiengine.sounds/Editor/Inspector/Sounds/AudioPlayerEditor.cs
+++ Packages/com.teamdodoco.runiengine.sounds/Editor/Inspector/Sounds/AudioPlayerEditor.cs
@@ -2,6 +2,7 @@
 using RuniEngine.Resource.Sounds;
 using RuniEngine.Sounds;
 using System;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,6 +12,56 @@ namespace RuniEngine.Editor.Inspector.Sounds
     [CustomEditor(typeof(AudioPlayer))]
     public class AudioPlayerEditor : SoundPlayerBaseEditor
     {
+        static Type? _audioFilterGUIType;
+        public static Type audioFilterGUIType => _audioFilterGUIType ??= editorAssembly.GetType("UnityEditor.AudioFilterGUI");
+
+
+        static object? _audioFilterGUIInstance;
+        public static object audioFilterGUIInstance => _audioFilterGUIInstance ??= Activator.CreateInstance(_audioFilterGUIType);
+
+
+        static MethodInfo? _audioFilterGUIMethod;
+        public static MethodInfo audioFilterGUIMethod => _audioFilterGUIMethod ??= audioFilterGUIType.GetMethod("DrawAudioFilterGUI");
+
+
+
+        static Type? _audioUtilType;
+        public static Type audioUtilType => _audioUtilType ??= editorAssembly.GetType("UnityEditor.AudioUtil");
+
+
+        static MethodInfo? _audioUtilHasAudioCallbackMethod;
+        public static MethodInfo audioUtilHasAudioCallbackMethod => _audioUtilHasAudioCallbackMethod ??= audioUtilType.GetMethod("HasAudioCallback");
+
+
+        static MethodInfo? _audioUtilGetCustomFilterChannelCountMethod;
+        public static MethodInfo audioUtilGetCustomFilterChannelCountMethod => _audioUtilGetCustomFilterChannelCountMethod ??= audioUtilType.GetMethod("GetCustomFilterChannelCount");
+
+
+
+        public override void OnInspectorGUI()
+        {
+            if (target == null)
+                return;
+
+            base.OnInspectorGUI();
+
+            object[] monos = new object[] { target };
+            if ((bool)audioUtilHasAudioCallbackMethod.Invoke(null, monos) && ((int)audioUtilGetCustomFilterChannelCountMethod.Invoke(null, monos)) > 0)
+            {
+                DrawLine();
+
+                try
+                {
+                    audioFilterGUIMethod.Invoke(audioFilterGUIInstance, new object[] { target });
+                }
+                catch (TargetInvocationException e)
+                {
+                    if (e.InnerException.GetType() != typeof(NullReferenceException))
+                        throw;
+                }
+            }
+        }
+
         protected override void NameSpaceKeyGUI()
         {
             if (target == null)
