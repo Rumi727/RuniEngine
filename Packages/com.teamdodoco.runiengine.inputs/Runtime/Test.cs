bb847a0ebd7b62086192833371495a1cb7dbbbe6
Rumi727 <jun32820@gmail.com>
2024-02-26 21:16:35 +0900

    위치 테스트 코드 복구

--- Packages/com.teamdodoco.runiengine.inputs/Runtime/Test.cs
+++ Packages/com.teamdodoco.runiengine.inputs/Runtime/Test.cs
@@ -1,19 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using RuniEngine.Inputs;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace RuniEngine
 {
     public class Test : MonoBehaviour
     {
+        Vector2 rotation = Vector2.zero;
+
         void Update()
         {
-            if (Vector3.zero != Input.mousePositionDelta)
+            float speed;
+            if (InputManager.GetKey(KeyCode.LeftControl))
+                speed = 0.25f * Kernel.fpsUnscaledSmoothDeltaTime;
+            else
+                speed = 0.125f * Kernel.fpsUnscaledSmoothDeltaTime;
+
             {
-                Debug.Log($"Input System : {Pointer.current.delta.value}");
-                Debug.Log($"Input : {Input.mousePositionDelta}");
+                Vector3 rotation = transform.localEulerAngles;
+                transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+
+                if (InputManager.GetKey(KeyCode.A))
+                    transform.position -= transform.right * speed;
+                if (InputManager.GetKey(KeyCode.D))
+                    transform.position += transform.right * speed;
+                if (InputManager.GetKey(KeyCode.S))
+                    transform.position -= transform.forward * speed;
+                if (InputManager.GetKey(KeyCode.W))
+                    transform.position += transform.forward * speed;
+                if (InputManager.GetKey(KeyCode.LeftShift))
+                    transform.position -= transform.up * speed;
+                if (InputManager.GetKey(KeyCode.Space))
+                    transform.position += transform.up * speed;
+
+                transform.localEulerAngles = rotation;
             }
+
+            if (InputManager.GetKey(KeyCode.Mouse0))
+            {
+                Vector2 rotation = InputManager.pointerPositionDelta * 0.5f;
+                this.rotation += new Vector2(-rotation.y, rotation.x);
+            }
+
+            transform.localEulerAngles = rotation;
         }
     }
 }
