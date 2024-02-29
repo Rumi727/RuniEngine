6b676a439c1bb7079d1b1d9641a26f5be4f2de52
Rumi727 <jun32820@gmail.com>
2024-03-01 07:33:42 +0900

    딕셔너리의 키를 교채하는 RenameKey 메소드 추가

--- Packages/com.teamdodoco.runiengine/Runtime/Utility/ListUtility.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Utility/ListUtility.cs
@@ -2134,5 +2134,13 @@ namespace RuniEngine
             }
         }
         #endregion
+
+        public static void RenameKey<TKey, TValue>(this IDictionary<TKey, TValue> dic, TKey fromKey, TKey toKey)
+        {
+            TValue value = dic[fromKey];
+
+            dic.Remove(fromKey);
+            dic[toKey] = value;
+        }
     }
 }
