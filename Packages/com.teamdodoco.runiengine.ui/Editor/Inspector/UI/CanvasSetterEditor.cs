514b0ecec2c5b7b046d8910513b51712e17a6abe
Rumi727 <jun32820@gmail.com>
2024-03-09 13:00:42 +0900

    에디터 페이드 애니메이션

--- Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/CanvasSetterEditor.cs
+++ Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/CanvasSetterEditor.cs
@@ -2,6 +2,7 @@
 using RuniEngine.Editor.Inspector.UI;
 using RuniEngine.UI;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,11 @@ namespace RuniEngine.Editor
     [CustomEditor(typeof(CanvasSetter))]
     public class CanvasSetterEditor : UIBaseEditor<CanvasSetter>
     {
+        readonly AnimBool disableScreenAreaAnim = new AnimBool();
+        readonly AnimBool worldRenderModeAnim = new AnimBool();
+        readonly AnimBool overlaySpaceAnim = new AnimBool();
+        readonly AnimBool disableUISizeAnim = new AnimBool();
+
         public override void OnInspectorGUI()
         {
             if (targets == null || targets.Length <= 0 || target == null)
@@ -18,39 +24,71 @@ namespace RuniEngine.Editor
 
             bool mixed = targets.Length > 1;
 
+            bool spaceMixed = !TargetsIsEquals(x => x.canvas != null ? x.canvas.renderMode : RenderMode.ScreenSpaceOverlay);
+
+            bool overlaySpace = (target.canvas != null ? target.canvas.renderMode : RenderMode.ScreenSpaceOverlay) == RenderMode.ScreenSpaceOverlay;
+            bool worldSpace = (target.canvas != null ? target.canvas.renderMode : RenderMode.ScreenSpaceOverlay) == RenderMode.WorldSpace;
+
             EditorGUI.BeginChangeCheck();
 
-            UseProperty("_disableScreenArea", TryGetText("inspector.canvas_setter.disableScreenArea"));
-
-            if (!target.disableScreenArea || mixed)
             {
-                UseProperty("_worldRenderMode", TryGetText("inspector.canvas_setter.worldRenderMode"));
+                UseProperty("_disableScreenArea", TryGetText("inspector.canvas_setter.disableScreenArea"));
 
-                if (target.worldRenderMode || mixed)
-                    UseProperty("_planeDistance", TryGetText("inspector.canvas_setter.planeDistance"));
+                disableScreenAreaAnim.FadeGroup(!target.disableScreenArea || !TargetsIsEquals(x => x.disableScreenArea, targets), Repaint, () =>
+                {
+                    {
+                        UseProperty("_worldRenderMode", TryGetText("inspector.canvas_setter.worldRenderMode"));
 
-                Space();
+                        worldRenderModeAnim.FadeGroup(target.worldRenderMode || !TargetsIsEquals(x => x.worldRenderMode), Repaint, () =>
+                            UseProperty("_planeDistance", TryGetText("inspector.canvas_setter.planeDistance"))
+                        );
+                    }
 
-                UseProperty("_areaOffset", TryGetText("inspector.canvas_setter.areaOffset"));
+                    Space();
 
-                Space();
+                    {
+                        UseProperty("_areaOffset", TryGetText("inspector.canvas_setter.areaOffset"));
 
-                UseProperty("_globalScreenPositionMultiple", TryGetText("inspector.canvas_setter.globalScreenPositionMultiple"));
-                UseProperty("_globalScreenAreaMultiple", TryGetText("inspector.canvas_setter.globalScreenAreaMultiple"));
+                        overlaySpaceAnim.FadeGroup(overlaySpace || spaceMixed, Repaint, () =>
+                        {
+                            Space();
+
+                            UseProperty("_globalScreenPositionMultiple", TryGetText("inspector.canvas_setter.globalScreenPositionMultiple"));
+                            UseProperty("_globalScreenAreaMultiple", TryGetText("inspector.canvas_setter.globalScreenAreaMultiple"));
+                        });
+                    }
+                });
             }
 
             DrawLine();
 
-            UseProperty("_disableUISize", TryGetText("inspector.canvas_setter.disableUISize"));
-            if (!target.disableUISize || mixed)
             {
-                UseProperty("_uiSize", TryGetText("inspector.canvas_setter.uiSize"));
+                UseProperty("_disableUISize", TryGetText("inspector.canvas_setter.disableUISize"));
 
-                if (!mixed && target.GetComponent<CanvasScaler>() != null)
-                {
-                    Space();
-                    EditorGUILayout.HelpBox(TryGetText("inspector.canvas_setter_editor.error"), MessageType.Error);
-                }
+                disableUISizeAnim.FadeGroup
+                (
+                    !target.disableUISize && (!worldSpace || spaceMixed || target.worldRenderMode || !TargetsIsEquals(x => x.worldRenderMode)),
+                    Repaint,
+                    () =>
+                    {
+                        UseProperty("_uiSize", TryGetText("inspector.canvas_setter.uiSize"));
+
+                        for (int i = 0; i < targets.Length; i++)
+                        {
+                            CanvasSetter? setter = targets[i];
+                            if (setter == null)
+                                continue;
+
+                            if (setter.TryGetComponent<CanvasScaler?>(out _))
+                            {
+                                Space();
+                                EditorGUILayout.HelpBox(TryGetText("inspector.canvas_setter_editor.error"), MessageType.Error);
+
+                                break;
+                            }
+                        }
+                    }
+                );
             }
 
             DrawLine();
@@ -58,30 +96,19 @@ namespace RuniEngine.Editor
             UseProperty("_alwaysVisible", TryGetText("inspector.canvas_setter.alwaysVisible"));
 
             if (EditorGUI.EndChangeCheck())
-            {
-                for (int i = 0; i < targets.Length; i++)
-                {
-                    CanvasSetter? canvasSetter = targets[i];
-                    if (canvasSetter != null)
-                        canvasSetter.SetDirty();
-                }
-            }
+                TargetsInvoke(x => x.SetDirty());
 
             DrawLine();
 
             if (GUILayout.Button(TryGetText("gui.delete"), GUILayout.ExpandWidth(false)) && targets != null)
             {
-                for (int i = 0; i < targets.Length; i++)
+                TargetsInvoke(x =>
                 {
-                    CanvasSetter? tool = targets[i];
-                    if (tool == null)
-                        continue;
+                    Canvas? canvas = x.canvas;
 
-                    Canvas? canvas = tool.canvas;
-
-                    DestroyImmediate(tool);
+                    DestroyImmediate(x);
                     Undo.DestroyObjectImmediate(canvas);
-                }
+                });
             }
         }
     }
