fd04e19a81d6a2e03c27b8c2e2a48ea5d45b505d
Rumi727 <jun32820@gmail.com>
2024-03-10 03:00:41 +0900

    애니메이션 추가

--- Packages/com.teamdodoco.runiengine/Editor/Inspector/CameraSetterEditor.cs
+++ Packages/com.teamdodoco.runiengine/Editor/Inspector/CameraSetterEditor.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using RuniEngine.Editor.Inspector;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 
 namespace RuniEngine.Editor
 {
@@ -8,6 +9,7 @@ namespace RuniEngine.Editor
     [CustomEditor(typeof(CameraSetter))]
     public class CameraSetterEditor : CustomInspectorBase<CameraSetter>
     {
+        AnimBool? disableAnim;
         public override void OnInspectorGUI()
         {
             if (target == null || targets == null)
@@ -15,29 +17,29 @@ namespace RuniEngine.Editor
 
             UseProperty("_disable", TryGetText("gui.disable"));
 
-            if (target.disable && TargetsIsEquals(x => x.disable))
-                return;
-
-            Space();
-
-            EditorGUI.BeginChangeCheck();
-
-            UseProperty("_normalizedViewPortRect", TryGetText("inspector.camera_setter.normalizedViewPortRect"));
-
-            Space();
-
-            UseProperty("_globalScreenPositionMultiple", TryGetText("inspector.camera_setter.globalScreenPositionMultiple"));
-            UseProperty("_globalScreenAreaMultiple", TryGetText("inspector.camera_setter.globalScreenAreaMultiple"));
-
-            if (EditorGUI.EndChangeCheck())
+            FadeGroup(ref disableAnim, Repaint, !target.disable || !TargetsIsEquals(x => x.disable), () =>
             {
-                for (int i = 0; i < targets.Length; i++)
+                Space();
+
+                EditorGUI.BeginChangeCheck();
+
+                UseProperty("_normalizedViewPortRect", TryGetText("inspector.camera_setter.normalizedViewPortRect"));
+
+                Space();
+
+                UseProperty("_globalScreenPositionMultiple", TryGetText("inspector.camera_setter.globalScreenPositionMultiple"));
+                UseProperty("_globalScreenAreaMultiple", TryGetText("inspector.camera_setter.globalScreenAreaMultiple"));
+
+                if (EditorGUI.EndChangeCheck())
                 {
-                    CameraSetter? cameraSetter = targets[i];
-                    if (cameraSetter != null)
-                        cameraSetter.SetDirty();
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+                        CameraSetter? cameraSetter = targets[i];
+                        if (cameraSetter != null)
+                            cameraSetter.SetDirty();
+                    }
                 }
-            }
+            });
         }
     }
 }
