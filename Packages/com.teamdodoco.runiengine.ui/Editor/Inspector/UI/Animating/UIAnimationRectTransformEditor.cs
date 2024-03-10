0006c798dc8f26a5e6d324544315584ea57b0a38
Rumi727 <jun32820@gmail.com>
2024-03-10 12:58:16 +0900

    애니메이션 추가

--- Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/Animating/UIAnimationRectTransformEditor.cs
+++ Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/Animating/UIAnimationRectTransformEditor.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using RuniEngine.UI.Animating;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 
 namespace RuniEngine.Editor.Inspector.UI.Animating
 {
@@ -8,6 +9,45 @@ namespace RuniEngine.Editor.Inspector.UI.Animating
     [CustomEditor(typeof(UIAnimationRectTransform))]
     public class UIAnimationRectTransformEditor : UIAnimationEditor<UIAnimationRectTransform>
     {
+        AnimBool? anchoredPositionAnim;
+        AnimBool? anchoredPositionXAnim;
+        AnimBool? anchoredPositionYAnim;
+        AnimBool? anchoredPositionZAnim;
+
+        AnimBool? sizeDeltaAnim;
+        AnimBool? sizeDeltaXAnim;
+        AnimBool? sizeDeltaYAnim;
+
+        AnimBool? offsetMinAnim;
+        AnimBool? offsetMinXAnim;
+        AnimBool? offsetMinYAnim;
+
+        AnimBool? offsetMaxAnim;
+        AnimBool? offsetMaxXAnim;
+        AnimBool? offsetMaxYAnim;
+
+        AnimBool? anchorMinAnim;
+        AnimBool? anchorMinXAnim;
+        AnimBool? anchorMinYAnim;
+
+        AnimBool? anchorMaxAnim;
+        AnimBool? anchorMaxXAnim;
+        AnimBool? anchorMaxYAnim;
+
+        AnimBool? pivotAnim;
+        AnimBool? pivotXAnim;
+        AnimBool? pivotYAnim;
+
+        AnimBool? rotationAnim;
+        AnimBool? rotationXAnim;
+        AnimBool? rotationYAnim;
+        AnimBool? rotationZAnim;
+
+        AnimBool? scaleAnim;
+        AnimBool? scaleXAnim;
+        AnimBool? scaleYAnim;
+        AnimBool? scaleZAnim;
+
         public override void OnInspectorGUI()
         {
             if (target == null)
@@ -19,62 +59,54 @@ namespace RuniEngine.Editor.Inspector.UI.Animating
             bool modeMixed = !TargetsIsEquals(x => x.mode);
             UseProperty("_mode", TryGetText("gui.mode"));
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionY) || target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionZ))
-                Space();
+            FadeGroup(ref anchoredPositionAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionY) || target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionZ), () => Space());
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionX) || modeMixed, "_anchoredPositionX", TryGetText("rect_transform.anchored_position.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionY) || modeMixed, "_anchoredPositionY", TryGetText("rect_transform.anchored_position.y"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionZ) || modeMixed, "_anchoredPositionZ", TryGetText("rect_transform.anchored_position.z"));
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaX) || target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaY))
-                Space();
+            AutoField(ref anchoredPositionXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionX) || modeMixed, "_anchoredPositionX", TryGetText("rect_transform.anchored_position.x"));
+            AutoField(ref anchoredPositionYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionY) || modeMixed, "_anchoredPositionY", TryGetText("rect_transform.anchored_position.y"));
+            AutoField(ref anchoredPositionZAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchoredPositionZ) || modeMixed, "_anchoredPositionZ", TryGetText("rect_transform.anchored_position.z"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaX) || modeMixed, "_sizeDeltaX", TryGetText("rect_transform.size_delta.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaY) || modeMixed, "_sizeDeltaY", TryGetText("rect_transform.size_delta.y"));
+            FadeGroup(ref sizeDeltaAnim, target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaX) || target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinX) || target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinY))
-                Space();
+            AutoField(ref sizeDeltaXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaX) || modeMixed, "_sizeDeltaX", TryGetText("rect_transform.size_delta.x"));
+            AutoField(ref sizeDeltaYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.sizeDeltaY) || modeMixed, "_sizeDeltaY", TryGetText("rect_transform.size_delta.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinX) || modeMixed, "_offsetMinX", TryGetText("rect_transform.offset_min.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinY) || modeMixed, "_offsetMinY", TryGetText("rect_transform.offset_min.y"));
+            FadeGroup(ref offsetMinAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinX) || target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxX) || target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxY))
-                Space();
+            AutoField(ref offsetMinXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinX) || modeMixed, "_offsetMinX", TryGetText("rect_transform.offset_min.x"));
+            AutoField(ref offsetMinYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMinY) || modeMixed, "_offsetMinY", TryGetText("rect_transform.offset_min.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxX) || modeMixed, "_offsetMaxX", TryGetText("rect_transform.offset_max.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxY) || modeMixed, "_offsetMaxY", TryGetText("rect_transform.offset_max.y"));
+            FadeGroup(ref offsetMaxAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxX) || target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinY))
-                Space();
+            AutoField(ref offsetMaxXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxX) || modeMixed, "_offsetMaxX", TryGetText("rect_transform.offset_max.x"));
+            AutoField(ref offsetMaxYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.offsetMaxY) || modeMixed, "_offsetMaxY", TryGetText("rect_transform.offset_max.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinX) || modeMixed, "_anchorMinX", TryGetText("rect_transform.anchor_min.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinY) || modeMixed, "_anchorMinY", TryGetText("rect_transform.anchor_min.y"));
+            FadeGroup(ref anchorMinAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxY))
-                Space();
+            AutoField(ref anchorMinXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinX) || modeMixed, "_anchorMinX", TryGetText("rect_transform.anchor_min.x"));
+            AutoField(ref anchorMinYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMinY) || modeMixed, "_anchorMinY", TryGetText("rect_transform.anchor_min.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxX) || modeMixed, "_anchorMaxX", TryGetText("rect_transform.anchor_max.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxY) || modeMixed, "_anchorMaxY", TryGetText("rect_transform.anchor_max.y"));
+            FadeGroup(ref anchorMaxAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxX) || target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.pivotX) || target.mode.HasFlag(UIAnimationRectTransformMode.pivotY))
-                Space();
+            AutoField(ref anchorMaxXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxX) || modeMixed, "_anchorMaxX", TryGetText("rect_transform.anchor_max.x"));
+            AutoField(ref anchorMaxYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.anchorMaxY) || modeMixed, "_anchorMaxY", TryGetText("rect_transform.anchor_max.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.pivotX) || modeMixed, "_pivotX", TryGetText("rect_transform.pivot.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.pivotY) || modeMixed, "_pivotY", TryGetText("rect_transform.pivot.y"));
+            FadeGroup(ref pivotAnim, target.mode.HasFlag(UIAnimationRectTransformMode.pivotX) || target.mode.HasFlag(UIAnimationRectTransformMode.pivotY), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.rotationX) || target.mode.HasFlag(UIAnimationRectTransformMode.rotationY) || target.mode.HasFlag(UIAnimationRectTransformMode.rotationZ))
-                Space();
+            AutoField(ref pivotXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.pivotX) || modeMixed, "_pivotX", TryGetText("rect_transform.pivot.x"));
+            AutoField(ref pivotYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.pivotY) || modeMixed, "_pivotY", TryGetText("rect_transform.pivot.y"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.rotationX) || modeMixed, "_rotationX", TryGetText("transform.rotation.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.rotationY) || modeMixed, "_rotationY", TryGetText("transform.rotation.y"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.rotationZ) || modeMixed, "_rotationZ", TryGetText("transform.rotation.z"));
+            FadeGroup(ref rotationAnim, target.mode.HasFlag(UIAnimationRectTransformMode.rotationX) || target.mode.HasFlag(UIAnimationRectTransformMode.rotationY) || target.mode.HasFlag(UIAnimationRectTransformMode.rotationZ), () => Space());
 
-            if (target.mode.HasFlag(UIAnimationRectTransformMode.scaleX) || target.mode.HasFlag(UIAnimationRectTransformMode.scaleY) || target.mode.HasFlag(UIAnimationRectTransformMode.scaleZ))
-                Space();
+            AutoField(ref rotationXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.rotationX) || modeMixed, "_rotationX", TryGetText("transform.rotation.x"));
+            AutoField(ref rotationYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.rotationY) || modeMixed, "_rotationY", TryGetText("transform.rotation.y"));
+            AutoField(ref rotationZAnim, target.mode.HasFlag(UIAnimationRectTransformMode.rotationZ) || modeMixed, "_rotationZ", TryGetText("transform.rotation.z"));
 
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.scaleX) || modeMixed, "_scaleX", TryGetText("transform.scale.x"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.scaleY) || modeMixed, "_scaleY", TryGetText("transform.scale.y"));
-            AutoField(target.mode.HasFlag(UIAnimationRectTransformMode.scaleZ) || modeMixed, "_scaleZ", TryGetText("transform.scale.z"));
+            FadeGroup(ref scaleAnim, target.mode.HasFlag(UIAnimationRectTransformMode.scaleX) || target.mode.HasFlag(UIAnimationRectTransformMode.scaleY) || target.mode.HasFlag(UIAnimationRectTransformMode.scaleZ), () => Space());
+
+            AutoField(ref scaleXAnim, target.mode.HasFlag(UIAnimationRectTransformMode.scaleX) || modeMixed, "_scaleX", TryGetText("transform.scale.x"));
+            AutoField(ref scaleYAnim, target.mode.HasFlag(UIAnimationRectTransformMode.scaleY) || modeMixed, "_scaleY", TryGetText("transform.scale.y"));
+            AutoField(ref scaleZAnim, target.mode.HasFlag(UIAnimationRectTransformMode.scaleZ) || modeMixed, "_scaleZ", TryGetText("transform.scale.z"));
         }
     }
 }
--- Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/Animating/UIAnimationTransformEditor.cs
+++ Packages/com.teamdodoco.runiengine.ui/Editor/Inspector/UI/Animating/UIAnimationTransformEditor.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using RuniEngine.UI.Animating;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
 
 namespace RuniEngine.Editor.Inspector.UI.Animating
 {
@@ -8,6 +9,21 @@ namespace RuniEngine.Editor.Inspector.UI.Animating
     [CustomEditor(typeof(UIAnimationTransform))]
     public class UIAnimationTransformEditor : UIAnimationEditor<UIAnimationTransform>
     {
+        AnimBool? positionAnim;
+        AnimBool? positionXAnim;
+        AnimBool? positionYAnim;
+        AnimBool? positionZAnim;
+
+        AnimBool? rotationAnim;
+        AnimBool? rotationXAnim;
+        AnimBool? rotationYAnim;
+        AnimBool? rotationZAnim;
+
+        AnimBool? scaleAnim;
+        AnimBool? scaleXAnim;
+        AnimBool? scaleYAnim;
+        AnimBool? scaleZAnim;
+
         public override void OnInspectorGUI()
         {
             if (target == null)
@@ -19,26 +35,23 @@ namespace RuniEngine.Editor.Inspector.UI.Animating
             bool modeMixed = !TargetsIsEquals(x => x.mode);
             UseProperty("_mode", TryGetText("gui.mode"));
 
-            if (target.mode.HasFlag(UIAnimationTransformMode.positionX) || target.mode.HasFlag(UIAnimationTransformMode.positionY) || target.mode.HasFlag(UIAnimationTransformMode.positionZ))
-                Space();
+            FadeGroup(ref positionAnim, target.mode.HasFlag(UIAnimationTransformMode.positionX) || target.mode.HasFlag(UIAnimationTransformMode.positionY) || target.mode.HasFlag(UIAnimationTransformMode.positionZ), () => Space());
 
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.positionX) || modeMixed, "_positionX", TryGetText("transform.position.x"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.positionY) || modeMixed, "_positionY", TryGetText("transform.position.y"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.positionZ) || modeMixed, "_positionZ", TryGetText("transform.position.z"));
+            AutoField(ref positionXAnim, target.mode.HasFlag(UIAnimationTransformMode.positionX) || modeMixed, "_positionX", TryGetText("transform.position.x"));
+            AutoField(ref positionYAnim, target.mode.HasFlag(UIAnimationTransformMode.positionY) || modeMixed, "_positionY", TryGetText("transform.position.y"));
+            AutoField(ref positionZAnim, target.mode.HasFlag(UIAnimationTransformMode.positionZ) || modeMixed, "_positionZ", TryGetText("transform.position.z"));
 
-            if (target.mode.HasFlag(UIAnimationTransformMode.rotationX) || target.mode.HasFlag(UIAnimationTransformMode.rotationY) || target.mode.HasFlag(UIAnimationTransformMode.rotationZ))
-                Space();
+            FadeGroup(ref rotationAnim, target.mode.HasFlag(UIAnimationTransformMode.rotationX) || target.mode.HasFlag(UIAnimationTransformMode.rotationY) || target.mode.HasFlag(UIAnimationTransformMode.rotationZ), () => Space());
 
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.rotationX) || modeMixed, "_rotationX", TryGetText("transform.rotation.x"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.rotationY) || modeMixed, "_rotationY", TryGetText("transform.rotation.y"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.rotationZ) || modeMixed, "_rotationZ", TryGetText("transform.rotation.z"));
+            AutoField(ref rotationXAnim, target.mode.HasFlag(UIAnimationTransformMode.rotationX) || modeMixed, "_rotationX", TryGetText("transform.rotation.x"));
+            AutoField(ref rotationYAnim, target.mode.HasFlag(UIAnimationTransformMode.rotationY) || modeMixed, "_rotationY", TryGetText("transform.rotation.y"));
+            AutoField(ref rotationZAnim, target.mode.HasFlag(UIAnimationTransformMode.rotationZ) || modeMixed, "_rotationZ", TryGetText("transform.rotation.z"));
 
-            if (target.mode.HasFlag(UIAnimationTransformMode.scaleX) || target.mode.HasFlag(UIAnimationTransformMode.scaleY) || target.mode.HasFlag(UIAnimationTransformMode.scaleZ))
-                Space();
+            FadeGroup(ref scaleAnim, target.mode.HasFlag(UIAnimationTransformMode.scaleX) || target.mode.HasFlag(UIAnimationTransformMode.scaleY) || target.mode.HasFlag(UIAnimationTransformMode.scaleZ), () => Space());
 
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.scaleX) || modeMixed, "_scaleX", TryGetText("transform.scale.x"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.scaleY) || modeMixed, "_scaleY", TryGetText("transform.scale.y"));
-            AutoField(target.mode.HasFlag(UIAnimationTransformMode.scaleZ) || modeMixed, "_scaleZ", TryGetText("transform.scale.z"));
+            AutoField(ref scaleXAnim, target.mode.HasFlag(UIAnimationTransformMode.scaleX) || modeMixed, "_scaleX", TryGetText("transform.scale.x"));
+            AutoField(ref scaleYAnim, target.mode.HasFlag(UIAnimationTransformMode.scaleY) || modeMixed, "_scaleY", TryGetText("transform.scale.y"));
+            AutoField(ref scaleZAnim, target.mode.HasFlag(UIAnimationTransformMode.scaleZ) || modeMixed, "_scaleZ", TryGetText("transform.scale.z"));
         }
     }
 }
