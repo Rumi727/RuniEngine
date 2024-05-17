11c36275c73cd58640566b1d79e9f4784f2df3aa
Rumi727 <jun32820@gmail.com>
2024-05-18 08:36:23 +0900

    어트리뷰트에 AllowMultiple 제거

--- Packages/com.teamdodoco.runiengine/Runtime/Booting/AwakenAttribute.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Booting/AwakenAttribute.cs
@@ -6,6 +6,6 @@ namespace RuniEngine.Booting
     /// <summary>
     /// 프로젝트 설정이 로드되기 전에 메소드를 호출 시켜주는 어트리뷰트 입니다
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Method)]
     public class AwakenAttribute : Attribute { }
 }
--- Packages/com.teamdodoco.runiengine/Runtime/Booting/StartenAttribute.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Booting/StartenAttribute.cs
@@ -6,6 +6,6 @@ namespace RuniEngine.Booting
     /// <summary>
     /// 프로젝트 설정 불러오기가 끝나면 메소드를 호출 시켜주는 어트리뷰트 입니다
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Method)]
     public class StartenAttribute : Attribute { }
 }
