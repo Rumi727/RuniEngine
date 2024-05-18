64c65110f8dddaaf99c893268a4055a03f50a523
Rumi727 <jun32820@gmail.com>
2024-05-18 10:38:19 +0900

    EditorGUIUtility.s_LastControlID 추가

---
+++ Packages/com.teamdodoco.runiengine.internal-api-bridge/Runtime/UnityEngine/GUIUtility.cs
@@ -0,0 +1,12 @@
+#nullable enable
+using System;
+
+namespace RuniEngine.APIBridge.UnityEngine
+{
+    public class GUIUtility
+    {
+        public static Type type { get; } = typeof(global::UnityEngine.GUIUtility);
+
+        protected GUIUtility() { }
+    }
+}
