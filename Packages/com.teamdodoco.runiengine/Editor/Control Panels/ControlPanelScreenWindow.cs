b8316165afa0e420f3201d145b370f3e56b846ad
Rumi727 <jun32820@gmail.com>
2024-05-26 17:42:10 +0900

    스크린 패널 업데이트

--- Packages/com.teamdodoco.runiengine/Editor/Control Panels/ControlPanelScreenWindow.cs
+++ Packages/com.teamdodoco.runiengine/Editor/Control Panels/ControlPanelScreenWindow.cs
@@ -1,6 +1,8 @@
 #nullable enable
+using RuniEngine.Editor.APIBridge.UnityEditor.UI;
 using RuniEngine.Screens;
 using UnityEditor;
+using UnityEngine;
 using static RuniEngine.Editor.EditorTool;
 
 namespace RuniEngine.Editor
@@ -11,7 +13,7 @@ namespace RuniEngine.Editor
         public int sort => 600;
 
         public bool allowUpdate => true;
-        public bool allowEditorUpdate => false;
+        public bool allowEditorUpdate => true;
 
         static ScreenMover? screenMover;
         static ScreenCroper? screenCroper;
@@ -26,6 +28,24 @@ namespace RuniEngine.Editor
             screenCroper.offset = RectOffsetField(TryGetText("gui.offset"), screenCroper.offset);
 
             EndWideMode();
+
+            DrawLine();
+
+            GUILayout.Label($"{TryGetText("gui.current_position")} - {ScreenManager.screenPosition.x} {ScreenManager.screenPosition.y} {ScreenManager.screenPosition.z}");
+            GUILayout.Label($"{TryGetText("gui.current_offset")} - {ScreenManager.screenArea.left} {ScreenManager.screenArea.right} {ScreenManager.screenArea.top} {ScreenManager.screenArea.bottom}");
+
+            Rect offset = new Rect(screenMover.position.x + screenCroper.offset.left, screenMover.position.y + screenCroper.offset.bottom, screenCroper.offset.right, screenCroper.offset.top);
+
+            Rect inner = Rect.zero;
+            
+            inner.x = offset.x;
+            inner.y = offset.y;
+            inner.width = (ScreenManager.width + offset.width - screenCroper.offset.left);
+            inner.height = (ScreenManager.height + offset.height - screenCroper.offset.bottom);
+
+            Space();
+
+            SpriteDrawUtility.DrawSprite(Texture2D.whiteTexture, EditorGUILayout.GetControlRect(false, 200), Vector4.zero, new Rect(0, 0, ScreenManager.width, ScreenManager.height), inner, Rect.zero, new Color(0.5f, 0.5f, 0.5f, 0.75f), null);
         }
     }
 }
