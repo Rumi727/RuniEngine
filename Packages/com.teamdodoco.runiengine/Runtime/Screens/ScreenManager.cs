709d7e5d0e2c1479d0e7a31f56f49b08b94384b0
Rumi727 <jun32820@gmail.com>
2024-05-26 17:41:40 +0900

    버그 수정

--- Packages/com.teamdodoco.runiengine/Runtime/Screens/ScreenManager.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Screens/ScreenManager.cs
@@ -6,57 +6,15 @@ namespace RuniEngine.Screens
 {
     public static class ScreenManager
     {
-        static int _width;
-        public static int width
-        {
-            get
-            {
-                if (Kernel.isPlaying)
-                    return _width;
-                else
-                    return Screen.width;
-            }
-        }
-
-        static int _height;
-        public static int height
-        {
-            get
-            {
-                if (Kernel.isPlaying)
-                    return _height;
-                else
-                    return Screen.height;
-            }
-        }
+        public static int width { get; private set; }
+        public static int height { get; private set; }
 
 
 
         public static Vector2Int size => new Vector2Int(width, height);
 
-        static Resolution _currentResolution;
-        public static Resolution currentResolution
-        {
-            get
-            {
-                if (Kernel.isPlaying)
-                    return _currentResolution;
-                else
-                    return Screen.currentResolution;
-            }
-        }
-
-        static Resolution[] _resolutions = new Resolution[0];
-        public static Resolution[] resolutions
-        {
-            get
-            {
-                if (Kernel.isPlaying)
-                    return _resolutions;
-                else
-                    return Screen.resolutions;
-            }
-        }
+        public static Resolution currentResolution { get; private set; } = new Resolution();
+        public static Resolution[] resolutions { get; private set; } = new Resolution[0];
 
 
 
@@ -104,15 +62,43 @@ namespace RuniEngine.Screens
 
 
         [Awaken]
-        static void Awaken() => CustomPlayerLoopSetter.initEvent += Update;
+        static void Awaken()
+        {
+            CustomPlayerLoopSetter.initEvent += Update;
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.update -= Update;
+            Kernel.quitting += () => UnityEditor.EditorApplication.update += Update;
+#endif
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+        static void InitializeOnLoadMethod()
+        {
+            if (!Kernel.isPlaying)
+                UnityEditor.EditorApplication.update += Update;
+        }
+#endif
 
         static void Update()
         {
-            _width = Screen.width;
-            _height = Screen.height;
+            if (Kernel.isPlaying)
+            {
+                width = Screen.width;
+                height = Screen.height;
+            }
+            else
+            {
+#if UNITY_EDITOR
+                string[] res = UnityEditor.UnityStats.screenRes.Split('x');
+                width = int.Parse(res[0]);
+                height = int.Parse(res[1]);
+#endif
+            }
 
-            _currentResolution = Screen.currentResolution;
-            _resolutions = Screen.resolutions;
+            currentResolution = Screen.currentResolution;
+            resolutions = Screen.resolutions;
         }
     }
 }
