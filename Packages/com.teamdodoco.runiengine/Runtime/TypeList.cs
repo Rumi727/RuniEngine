7dd2b1c9028bce09513462b3dda12cc28fb23439
Rumi727 <jun32820@gmail.com>
2024-05-29 21:19:28 +0900

    TypeList 추가
    리플랙션에서 제네릭 리스트의 타입을 보다 쉽게 가져오게 하기 위함

---
+++ Packages/com.teamdodoco.runiengine/Runtime/TypeList.cs
@@ -0,0 +1,15 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+
+namespace RuniEngine
+{
+    public class TypeList<T> : List<T>, ITypeList
+    {
+        public TypeList() : base() { }
+        public TypeList(int capacty) : base(capacty) { }
+        public TypeList(IEnumerable<T> collection) : base(collection) { }
+
+        public Type listType => typeof(T);
+    }
+}
