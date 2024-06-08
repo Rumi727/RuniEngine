bd68e2edf17d3b94900a8df5a4e1d1abf9d6b62e
Rumi727 <jun32820@gmail.com>
2024-06-08 23:03:14 +0900

    Update EditorStaticTool.Serialize.cs

--- Packages/com.teamdodoco.runiengine/Editor/Utility/EditorStaticTool.Serialize.cs
+++ Packages/com.teamdodoco.runiengine/Editor/Utility/EditorStaticTool.Serialize.cs
@@ -28,6 +28,31 @@ namespace RuniEngine.Editor
             }
         }
 
-        public static bool IsNullable(SerializedProperty serializedProperty) => serializedProperty.propertyType == SerializedPropertyType.ManagedReference || serializedProperty.propertyType == SerializedPropertyType.ObjectReference || serializedProperty.propertyType == SerializedPropertyType.ExposedReference || serializedProperty.propertyType == SerializedPropertyType.String;
+        public static bool IsNullable(this SerializedProperty serializedProperty) => serializedProperty.propertyType == SerializedPropertyType.ManagedReference || serializedProperty.propertyType == SerializedPropertyType.ObjectReference || serializedProperty.propertyType == SerializedPropertyType.ExposedReference || serializedProperty.propertyType == SerializedPropertyType.String;
+
+        public static SerializedProperty? GetParent(this SerializedProperty serializedProperty)
+        {
+            string path = serializedProperty.propertyPath;
+            if (path.Contains('.'))
+            {
+                int index = path.LastIndexOf('.');
+                path = path.Substring(0, index);
+
+                return serializedProperty.serializedObject.FindProperty(path);
+            }
+
+            return null;
+        }
+
+        public static bool IsInArray(this SerializedProperty? serializedProperty)
+        {
+            while ((serializedProperty = serializedProperty?.GetParent()) != null)
+            {
+                if (serializedProperty.isArray)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
