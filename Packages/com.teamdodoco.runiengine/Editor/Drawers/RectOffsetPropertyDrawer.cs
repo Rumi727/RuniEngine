0119e4ca3280fe771325e6ca42c5472b8948a991
Rumi727 <jun32820@gmail.com>
2024-07-31 17:52:36 +0900

    클래스 이름 수정

--- Packages/com.teamdodoco.runiengine/Editor/Drawers/RectOffsetPropertyDrawer.cs
+++ Packages/com.teamdodoco.runiengine/Editor/Drawers/RectOffsetPropertyDrawer.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 namespace RuniEngine.Editor.Drawers
 {
-    [CustomPropertyDrawer(typeof(Vector4))]
-    public class Vector4Drawer : PropertyDrawer
+    [CustomPropertyDrawer(typeof(RectOffset))]
+    public class RectOffsetPropertyDrawer : PropertyDrawer
     {
-        static readonly GUIContent[] labels = new GUIContent[] { new GUIContent("X"), new GUIContent("Y"), new GUIContent("Z"), new GUIContent("W") };
+        static readonly GUIContent[] labels = new GUIContent[] { new GUIContent("L"), new GUIContent("R"), new GUIContent("T"), new GUIContent("B") };
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             property.Next(true);
