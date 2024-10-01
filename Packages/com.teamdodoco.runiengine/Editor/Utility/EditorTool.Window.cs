c9d7c20d431edd0bc96b37145d4b824b61f2c54d
Rumi <jun32820@gmail.com>
2024-10-01 18:36:32 +0900

    코드상에서 호출된 창을 다시 그리는 메소드 추가

---
+++ Packages/com.teamdodoco.runiengine/Editor/Utility/EditorTool.Window.cs
@@ -0,0 +1,10 @@
+#nullable enable
+using RuniEngine.Editor.APIBridge.UnityEditor;
+
+namespace RuniEngine.Editor
+{
+    public partial class EditorTool
+    {
+        public static void RepaintCurrentWindow() => GUIView.current.Repaint();
+    }
+}
