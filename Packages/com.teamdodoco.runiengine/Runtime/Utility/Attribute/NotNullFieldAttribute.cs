e3f7a0314418b5f555301afbff089ac328076f36
Rumi <jun32820@gmail.com>
2024-10-01 18:37:22 +0900

    프로퍼티에도 사용할 수 있게 수정

--- Packages/com.teamdodoco.runiengine/Runtime/Utility/Attribute/NotNullFieldAttribute.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Utility/Attribute/NotNullFieldAttribute.cs
@@ -1,8 +1,10 @@
 #nullable enable
+using System;
 using UnityEngine;
 
 namespace RuniEngine
 {
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public sealed class NotNullFieldAttribute : PropertyAttribute
     {
 
