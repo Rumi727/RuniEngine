b59e47b53cb4e4b9e5b4de8d1fd622d6e866e2df
Rumi <jun32820@gmail.com>
2024-10-05 07:03:12 +0900

    IndexOf Linq 추가

--- Packages/com.teamdodoco.runiengine/Runtime/Utility/LinqExtras.cs
+++ Packages/com.teamdodoco.runiengine/Runtime/Utility/LinqExtras.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.Collections.Generic;
 
 namespace System.Linq
@@ -9,5 +10,26 @@ namespace System.Linq
 
         public static bool ContainsAll<T>(this IEnumerable<T> first, IEnumerable<T> second) => !second.Except(first).Any();
         public static bool ContainsAll<T>(this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> comparer) => !second.Except(first, comparer).Any();
+
+        public static int IndexOf<T>(this IEnumerable<T?> source, T? item)
+        {
+            int index = 0;
+            foreach (var item2 in source)
+            {
+                if (item == null && item2 == null)
+                    return index;
+                else if (item == null)
+                    continue;
+                else if (item2 == null)
+                    continue;
+
+                if (item.Equals(item2))
+                    return index;
+
+                index++;
+            }
+
+            return -1;
+        }
     }
 }
