d883332b7054273d907b5837977c2eb5517a0117
Rumi <jun32820@gmail.com>
2025-02-05 13:51:24 +0900

    Update SoundDataBase.cs

--- Packages/com.teamdodoco.runiengine.sounds/Runtime/Resource/Sounds/SoundDataBase.cs
+++ Packages/com.teamdodoco.runiengine.sounds/Runtime/Resource/Sounds/SoundDataBase.cs
@@ -1,9 +1,10 @@
 #nullable enable
 using Newtonsoft.Json;
+using System;
 
 namespace RuniEngine.Resource.Sounds
 {
-    public abstract class SoundData
+    public abstract class SoundData : IDisposable
     {
         public SoundData(string subtitle, bool isBGM)
         {
@@ -15,5 +16,11 @@ namespace RuniEngine.Resource.Sounds
         public bool isBGM { get; set; } = false;
 
         [JsonIgnore, NotNullField] public abstract SoundMetaDataBase[] sounds { get; }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < sounds.Length; i++)
+                sounds[i].Dispose();
+        }
     }
 }
